b02ca0ea817dc47799225281023648824a7589d4
ENZO FELIX CARRILHO <25132408@CFP127ALUNO.LOCAL>
2025-11-27 10:53:14 -0300

    feat: gets de status na model

--- model/dao/status_evento.js
+++ model/dao/status_evento.js
@@ -1,5 +1,5 @@
 /***********************************************************************************************************
- * Objetivo: Arquivo responsável pelo GET de dados no MySQL referente as UF'S
+ * Objetivo: Arquivo responsável pelo GET de dados no MySQL referente aos status de Eventos
  * Data: 27/11/2025
  * Autor: Enzo Carrilho
  * Versão: 1.0
@@ -11,10 +11,10 @@ const { PrismaClient } = require('../../generated/prisma')
 //Cria um novo objeto baseado na classe do PrismaClient
 const prisma = new PrismaClient()
 
-//Retorna todas as uf's
-const getAllUfs = async function(){
+//Retorna todos os status de Evento do BD
+const getAllEventStats = async function(){
     try{
-        let result = await prisma.$queryRaw(`select * from tb_uf order by id desc`)
+        let result = await prisma.$queryRaw(`select * from tb_status_evento order by id desc`)
         
         if(Array.isArray(result))
             return result
@@ -26,10 +26,10 @@ const getAllUfs = async function(){
     }
 }
 
-//Retorna uma UF filtrando por ID
-const getUfByID = async function(id){
+//Retorna um status de Evento filtrando por ID
+const getEventStatsByID = async function(id){
     try{
-        let result = await prisma.$queryRaw(`select * from tb_uf where id = ${id}`)
+        let result = await prisma.$queryRaw(`select * from tb_status_evento where id = ${id}`)
         
         if(Array.isArray(result))
             return result
@@ -39,9 +39,4 @@ const getUfByID = async function(id){
     }catch(error){
         return false
     }
-}
-
-module.exports = {
-    getAllUfs,
-    getUfByID
 }
