2f66cdd9474a42d0cdf2394a619df05170156951
ENZO FELIX CARRILHO <25132408@CFP127ALUNO.LOCAL>
2025-11-27 09:44:11 -0300

    feat: implementado controller dos gets de categoria

--- controller/categoria/controller_categoria.js
+++ controller/categoria/controller_categoria.js
@@ -5,20 +5,63 @@
  * Versão: 1.0
  ***********************************************************************************************************/
 const categoryDAO = require('../../model/dao/categoria.js')
+const DEFAULT_MESSAGES = require('../modulo/response_messages.js')
 
 const listCategorys = async function(){
+    //Criando um objeto para as mensagens
+    let MESSAGES = JSON.parse(JSON.stringify(DEFAULT_MESSAGES))
 
     try {
         let resultCategorys = await categoryDAO.getAllCategorys()
 
         if(resultCategorys){
             if(resultCategorys != null){
-                
+                MESSAGES.DEFAULT_HEADER.status = MESSAGES.SUCCESS_REQUEST.status
+                MESSAGES.DEFAULT_HEADER.status_code = MESSAGES.SUCCESS_REQUEST.status_code
+                MESSAGES.DEFAULT_HEADER.item = resultCategorys
+
+                return MESSAGES.DEFAULT_HEADER //200(sucesso)
+            }else{
+                return MESSAGES.ERROR_NOT_FOUND //404(não encontrado)
             }
+        }else{
+            return MESSAGES.ERROR_INTERNAL_SERVER_MODEL //500(erro interno)
         }
 
-
     } catch(error) {
+        return MESSAGES.ERROR_INTERNAL_SERVER_CONTROLLER //500(erro interno)
+    }
+}
+
+const listCategoryByID = async function name(id){
+    //Criando um objeto para as mensagens
+    let MESSAGES = JSON.parse(JSON.stringify(DEFAULT_MESSAGES))
+
+    try {
+        //Validação do ID
+        if(!isNaN(id) || id != '' || id > 0){
+            let resultCategory = await categoryDAO.getCategoryById(id)
+
+            if(resultCategory){
+
+                if(resultCategory != null){
+                    MESSAGES.DEFAULT_HEADER.status = MESSAGES.SUCCESS_REQUEST.status
+                    MESSAGES.DEFAULT_HEADER.status_code = MESSAGES.SUCCESS_REQUEST.status_code
+                    MESSAGES.DEFAULT_HEADER.item = resultCategory
+
+                    return MESSAGES.DEFAULT_HEADER //200(sucesso)
+                }else{
+                    return MESSAGES.ERROR_NOT_FOUND //404(não encontrado)
+                }
+            }else{
+                return MESSAGES.ERROR_INTERNAL_SERVER_MODEL //500(erro interno)
+            }
+        }else{
+            MESSAGES.ERROR_REQUIRED_FIELDS += ' [ID Inválido]'
+            return MESSAGES.ERROR_REQUIRED_FIELDS //400
+        }
         
+    } catch(error) {
+        return MESSAGES.ERROR_INTERNAL_SERVER_CONTROLLER //500(erro interno)
     }
 }
